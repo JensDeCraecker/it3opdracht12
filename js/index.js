d46e7a3d7903ad38a1321952a266b7780e49dcc9
JensDeCraecker <jens.decraecker@student.ap.be>
2017-01-16 13:38:54 +0100

    Back-up

---
+++ js/index.js
@@ -0,0 +1,90 @@
+var task1 = document.getElementById("task1");
+var task2 = document.getElementById("task2");
+var task3 = document.getElementById("task3");
+var task4 = document.getElementById("task4");
+var task5 = document.getElementById("task5");
+var task6 = document.getElementById("task6");
+var task7and8 = document.getElementById("task7and8");
+var task9 = document.getElementById("task9");
+var task10 = document.getElementById("task10");
+var task11 = document.getElementById("task11");
+var text = document.getElementsByClassName("hidden");
+
+task1.onmouseover = function (){
+  text[0].style.opacity = 1;
+   task1.onmouseout = function (){
+      text[0].style.opacity = 0;
+    }
+}
+
+
+task2.onmouseover = function (){
+  text[1].style.opacity = 1;
+   task2.onmouseout = function (){
+      text[1].style.opacity = 0;
+    }
+}
+
+
+task3.onmouseover = function (){
+  text[2].style.opacity = 1;
+    task3.onmouseout = function (){
+      text[2].style.opacity = 0;
+    }
+}
+
+
+task4.onmouseover = function (){
+  text[3].style.opacity = 1;
+    task4.onmouseout = function (){
+      text[3].style.opacity = 0;
+    }
+}
+
+
+task5.onmouseover = function (){
+  text[4].style.opacity = 1;
+    task5.onmouseout = function (){
+      text[4].style.opacity = 0;
+    }
+}
+
+
+task6.onmouseover = function (){
+  text[5].style.opacity = 1
+    task6.onmouseout = function (){
+      text[5].style.opacity = 0;
+    }
+}
+
+
+task7and8.onmouseover = function (){
+  text[6].style.opacity = 1;
+    task7and8.onmouseout = function (){
+      text[6].style.opacity = 0;
+    }
+}
+
+
+task9.onmouseover = function (){
+  text[7].style.opacity = 1;
+    task9.onmouseout = function (){
+      text[7].style.opacity = 0;
+    }
+}
+
+
+task10.onmouseover = function (){
+  text[8].style.opacity = 1;
+    task10.onmouseout = function (){
+      text[8].style.opacity = 0;
+    }
+}
+
+
+task11.onmouseover = function (){
+  text[9].style.opacity = 1;
+    task11.onmouseout = function (){
+      text[9].style.opacity = 0;
+    }
+}
